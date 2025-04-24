d77e19ec318754a07961f78dbfa5ba99d5a0719c
Pashalis Grivas <grivasp@piraeusbank.gr>
2025-04-25 00:33:34 +0300

    Add initial Flask app with basic routes and templates for Cloud & DevOps workshop

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/devops")
+def devops():
+    return render_template("devops.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
